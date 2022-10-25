ff531b951d9da14bbed742250b080df528b73326
Brian Bentancourt Martin <brian.bentancourt@sonda.com>
2022-10-25 17:33:51 -0300

    Agregar archivos de proyecto.

---
+++ ChatRoom/Controllers/ChatController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatRoom.Controllers
+{
+    public class ChatController : Controller
+    {
+       
+        public static Dictionary<int, string> Rooms = new Dictionary<int, string>()
+        {
+            { 1, "Dev"},
+            { 2, "Games"},
+            { 3, "Tutorials"}
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Room(int room)
+        {
+            return View("Room", room);
+        }
+    }
+}
